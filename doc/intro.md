ad7188d53c29c54ea52c43adf8b89b34bad12ff1
Adrian Medina <adrian.medina@mail.yu.edu>
2014-07-27 17:12:33 -0400

    init

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to mechanics
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject mechanics "0.1.0-SNAPSHOT"
+  :description "Scmutils in Clojure"
+  :url "http://github.com/aamedina/mechanics"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]])
